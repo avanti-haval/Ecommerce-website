becd222708380e50d807eb02ec608aa46eed61b7
Avanti <avanti.a.haval@gmail.com>
2024-06-17 20:44:54 +0530

    E-commerce Website Day5

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products,Order
+
+admin.site.site_header = "E-Commerce Website"
+admin.site.site_title = "Avanti Shopping"
+admin.site.index_title = "Manage Avanti Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+
+    change_category_to_default.short_description ='Default Category'
+    list_display = ('title','price','discount_price','category','description',)
+    search_fields = ('title','category','description',)
+    actions = ('change_category_to_default',)
+    fields = ('title','price',)
+    list_editable = ('price','category')
 
 # Register your models here.
-admin.site.register(Products)
+admin.site.register(Products,ProductAdmin)
+admin.site.register(Order)
